531950c2de1afdd0d7f7af485e336f875a7a9501
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-12-17 02:59:20 +0900

    first_commit

---
+++ app/assets/javascripts/box.js
@@ -0,0 +1,10 @@
+// $(document).on('turbolinks:load', function() {
+//   $(function(){
+//     $('.like-btn').on('click', function(){
+//       $('#title').css({
+//         'background':'rgba(200,200,200,0.5)',
+//         'height':'+=10'
+//       });
+//     })
+//   })
+// });
